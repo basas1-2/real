99f85c01cdcbcf7aecb7020361a43cccc4753048
muh-dev68 <sanniwahab23@gmail.com>
2025-10-27 16:49:56 +0000

    Added author field to Blog model

--- models/Blog.js
+++ models/Blog.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String },
-  content: { type: String },
-  imagePath: { type: String },
-}, { timestamps: true });
+const blogSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    imagePath: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Blog', blogSchema);
+module.exports = mongoose.model("Blog", blogSchema);
